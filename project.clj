226548aebe66ab2408fed0059813f429f8ea6a4d
Mathieu Gauthron <mathieu@matlux.net>
2014-04-29 11:09:50 +0100

    function can play white or black. added possibility to play against a human

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-chess-engine "0.1.0.5-SNAPSHOT"]]
+                 [clj-chess-engine "0.1.0.5"]]
   :main lambda-chess.core)
--- src/lambda_chess/core.clj
+++ src/lambda_chess/core.clj
@@ -1,13 +1,33 @@
 (ns lambda-chess.core
   (:require [clj-chess-engine.core :as chess]))
 
+;; my-fn is the var pointing onto the function we want to test
 (def my-fn
-  (fn random-f [{valid-moves :valid-moves am-i-white? :white-turn}]
+  ;;-----------------
+  ;; copy and past this anonymous function on lambda-zone.com
+  (fn [{:keys [board valid-moves am-i-white? in-check? history state]}]
                       (let [v (into [] valid-moves)]
                         (let [move (rand-int (count valid-moves))]
                           {:move (get v move), :state nil})))
+  ;;-----------------
 )
 
 
+(defn test-fn-vs-fn []
+  "play a white function against a black function"
+  (chess/trace-game-play {:white my-fn :black my-fn}))
+(defn test-fn-vs-human []
+  "play a white function against a human playing black"
+  (chess/trace-game-play {:white my-fn}))
+(defn test-human-vs-fn []
+  "play a human playing white against a black function"
+  (chess/trace-game-play {:black my-fn}))
+
+;; un comment the type of test you want to run
+;; i.e. do you want to play 2 functions agains one another or a function against an interactive prompt?
 (defn -main []
-  (chess/trace-game-play my-fn))
+  (test-fn-vs-fn)
+  ;;(test-fn-vs-human)
+  ;;(test-human-vs-fn)
+
+  )
